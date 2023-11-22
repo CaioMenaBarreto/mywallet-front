df092f6c7a7f13aa36997badcbae1719dbb3545f
Caio Lucas <clpmb2000@gmail.com>
2023-11-21 21:03:44 -0300

    feat: update and delete transactions

--- src/hooks/useQuickOut.jsx
+++ src/hooks/useQuickOut.jsx
@@ -1,18 +1,21 @@
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import AuthContext from "../contexts/authContexts";
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import AuthContext from "../contexts/AuthContext"
+import styled from "styled-components";
+import UsePromptLogin from "../components/usePrompt";
 
-export function useQuickOut(callback = null) {
+export default function useQuickOut() {
     const { userName, token } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [showPrompt, setShowPrompt] = useState(false);
 
     useEffect(() => {
         if (!token || !userName) {
-            if (callback) {
-                callback();
-            } else {
-                navigate('/');
-            }
+            // Se o usuário não estiver logado, exibe o componente UsePromptLogin
+            setShowPrompt(true);
         }
-    }, []);
+    }, [token, userName]);
+
+    // Retornar algo se necessário, por exemplo, null ou um componente padrão
+    return showPrompt ? <UsePromptLogin /> : null;
 }
--- src/services/transactions.jsx
+++ src/services/transactions.jsx
@@ -1,21 +1,62 @@
-import { useContext } from "react";
-import AuthContext from "../contexts/authContexts";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react"
+import AuthContext from "../contexts/AuthContext"
 import axios from "axios"
-import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function useGetTransactions() {
-    const { token } = useContext(AuthContext);
-    const [transactions, setTransactions] = useState(undefined);
-    const config = { headers: { Authorization: `Bearer ${token}` } };
+    const [transactions, setTransactions] = useState(undefined)
+    const { token } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const config = { headers: { Authorization: `Bearer ${token}` } }
+
+    function getTransactions() {
+        axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/transactions`, config)
+            .then(res => setTransactions(res.data))
+            .catch((err) => console.log(err.response.data))
+    }
+    
 
     useEffect(() => {
-        axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/transactions`, config)
-            .then(res => {
-                setTransactions(res.data);
-            })
-            .catch(err => console.error(err.response.data));
+        getTransactions()
     }, [])
 
-    return transactions;
+    return { transactions, getTransactions }
+}
+
+export function useAddTransaction() {
+    const { token } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const config = { headers: { Authorization: `Bearer ${token}` } }
+
+    return (body) => {
+        axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/transactions`, body, config)
+            .then(res => navigate("/home"))
+            .catch(err => alert(err.response.data))
+    }
+
+}
+
+export function useDeleteTransaction() {
+    const { token } = useContext(AuthContext)
+    const config = { headers: { Authorization: `Bearer ${token}` } }
+
+    return (id, getTransactions) => {
+        axios.delete(`${import.meta.env.VITE_REACT_APP_API_URL}/transactions/${id}`, config)
+            .then(res => getTransactions())
+            .catch(err => alert(err.response.data))
+    }
+}
+
+export function useEditTransaction() {
+    const { token } = useContext(AuthContext)
+    const config = { headers: { Authorization: `Bearer ${token}` } }
+    const navigate = useNavigate()
+
+    return (id, body) => {
+        axios.put(`${import.meta.env.VITE_REACT_APP_API_URL}/transactions/${id}`, body, config)
+            .then(res => navigate("/home"))
+            .catch(err => alert(err.response.data))
+    }
 }
